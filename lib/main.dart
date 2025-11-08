c09bae533cf571e79e89a84e5f284a758d139849
Ivan <ivancjanthoine@gmail.com>
2025-11-08 10:54:00 -0500

    Primer

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:bateria_application/ui/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bateria());
+}
+
---
+++ lib/ui/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bateria extends StatefulWidget {
+  const Bateria({super.key});
+
+  @override
+  State<Bateria> createState() => _BateriaState();
+}
+
+class _BateriaState extends State<Bateria> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
